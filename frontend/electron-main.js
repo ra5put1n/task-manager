60f15e78fa0fc84b2f2c6c6638d349bb2df45044
Kamal Kumar Khatri <keplerclan40@gmail.com>
2024-02-19 01:11:42 +0400

    Implemented Electron App

---
+++ frontend/electron-main.js
@@ -0,0 +1,50 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const { fileURLToPath } = require('url')
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const { BrowserWindow, app } = require('electron')
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const path = require('path')
+
+let mainWindow
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+async function createWindow() {
+    // Dynamically import the electron-is-dev module
+    const isDev = await import('electron-is-dev').then((module) => module.default)
+
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    })
+
+    const startURL = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, './dist/react/index.html')}`
+
+    mainWindow.loadURL(startURL)
+
+    mainWindow.on('closed', () => (mainWindow = null))
+}
+
+app.whenReady().then(() => {
+    createWindow()
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    // eslint-disable-next-line no-undef
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow()
+    }
+})
